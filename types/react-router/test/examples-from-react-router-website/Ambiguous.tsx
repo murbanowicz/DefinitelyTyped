bb81f10826ef2790f5c36f8c4088cdf79b0fc295
Marek Urbanowicz <marek@urbanowicz.dev>
2020-04-29 13:53:06 +0200

    breaking change

--- types/react-router/test/examples-from-react-router-website/Ambiguous.tsx
+++ types/react-router/test/examples-from-react-router-website/Ambiguous.tsx
@@ -1,12 +1,5 @@
 import * as React from 'react';
-import {
-  BrowserRouter as Router,
-  RouteComponentProps,
-  Route,
-  Link,
-  Routes,
-  useParams
-} from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Routes, useParams } from 'react-router-dom';
 
 const AmbiguousExample = () => (
   <Router>
